9937e3a2bf5528fecf1ef8ec23cb5c93eaffa15e
jkang331 <jkang331@uw.edu>
2016-04-22 21:58:15 -0700

    more sidebar bug fixing for hover and selection in the sidebar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,59 @@
 $(document).ready(function(){
     var mouseDownOccurred = false;
 
+    /********** Top Section Sidebar **********/
+    $("#file-view-icon").on("click", function() {
+        if ($(".FileDirectoryContent").is(':visible') == false) {
+            $(".FileDirectoryContent").show();
+            $(".FamilyViewContent").hide();
+            $("#family-view-icon").css('background-color', '#98CBC3'); //reset
+            $("#file-view-icon").css('background-color', '#4b9188'); //darken
+        } else {
+            $(".FileDirectoryContent").hide();
+            $("#file-view-icon").css('background-color', '#98CBC3'); //reset
+
+        }
+    });
+     
+    $("#family-view-icon").on("click", function() {
+        if ($(".FamilyViewContent").is(':visible') == false) {
+            $(".FamilyViewContent").show();
+            $(".FileDirectoryContent").hide();
+            $("#family-view-icon").css('background-color', '#4b9188'); //darken
+            $("#file-view-icon").css('background-color', '#98CBC3'); //reset
+        } else {
+            $(".FamilyViewContent").hide();
+            $("#family-view-icon").css('background-color', '#98CBC3'); //reset
+        }
+    });
+
+    /********** Bottom Section Sidebar **********/
+    $("#bio-graph-viz").on("click", function() {
+        if ($(".BioGraphViz").is(':visible') == false) {
+            $(".BioGraphViz").show();
+            $(".BarGraphViz").hide();
+            $("#bio-graph-viz").css('background-color', '#879e4e'); //darken 
+            $("#bar-graph-viz").css('background-color', '#A9C662'); //reset
+        } else {
+            $(".BioGraphViz").hide();
+            $("#bio-graph-viz").css('background-color', '#A9C662'); //reset
+
+        }
+    });
+     
+    $("#bar-graph-viz").on("click", function() {
+        if ($(".BarGraphViz").is(':visible') == false) {
+            $(".BarGraphViz").show();
+            $(".BioGraphViz").hide();
+            $("#bar-graph-viz").css('background-color', '#879e4e'); //darken
+            $("#bio-graph-viz").css('background-color', '#A9C662'); //reset
+        } else {
+            $(".BarGraphViz").hide();
+            $("#bar-graph-viz").css('background-color', '#A9C662'); //reset
+        }
+    });
+
+
     /********** Divider Bar actions **********/
     $('.hr').on('mousedown', function(e){
         e.preventDefault();
@@ -61,26 +114,6 @@ $(document).ready(function(){
         displayData(family, member).success(function(d) {
             $("#data-vis-body").append(d[0]);
         });
-    });
-
-
-    /********** Top Section Sidebar **********/
-    $("#file-view-icon").on("click", function() {
-        if ($("#file-view-wrap").is(':visible') == false) {
-            $("#file-view-wrap").show();
-            $("#family-view-wrap").hide();
-        } else {
-            $("#file-view-wrap").hide();
-        }
-    });
-     
-    $("#family-view-icon").on("click", function() {
-        if ($("#family-view-wrap").is(':visible') == false) {
-            $("#family-view-wrap").show();
-            $("#file-view-wrap").hide();
-        } else {
-            $("#family-view-wrap").hide();
-        }
     });
 });
 
